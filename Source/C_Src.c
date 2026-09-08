5d031e3dd1c606b9b5ed3ee6a3ef57afe7f8487e
ScMason90 <lwjlfmyj@outlook.com>
2026-09-08 13:59:52 +0800

    init commit

---
+++ Source/C_Src.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("C_Src.c: Hello, World! \n");
+
+    return 0;
+}
---
+++ Source/src.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "src.cpp: Hello, world!" << std::endl;
+    return 0;
+}
